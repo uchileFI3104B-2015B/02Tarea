2f9b96911b7068718a14382084e5517efa5869c4
maxidirk <maxidirk@yahoo.es>
2015-09-26 20:05:15 -0300

    Se crea archivo .py

---
+++ Tarea02.py
@@ -0,0 +1,10 @@
+#######################################################
+'''
+Tarea 2
+Maximiliano Dirk Vega Aguilera
+18.451.231-9
+'''
+#######################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
