91d40cb5af33ab5e1383546c4915fcaa91b94453
Maria Constanza Flores <tatiflores.4@gmail.com>
2015-09-27 21:09:47 -0300

    Declara la funcion y_suelo e y_pelota

---
+++ tarea2.py
@@ -0,0 +1,18 @@
+'''
+Este es el codigo es el de la tarea 2.
+Despues definire si voy a separar los codigos de cada pregunta
+o hacerlos todos aqui
+'''
+
+def y_pelota():
+    '''
+    Ecuacion de posicion (vertical) de la pelota en funcion
+    del tiempo
+    '''
+    pass
+
+def y_suelo():
+    '''
+    Ecuacion de posicion del suelo en funcion del tiempo
+    '''
+    pass
