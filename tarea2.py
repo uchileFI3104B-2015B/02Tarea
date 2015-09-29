8527db108938d483adac9a36ecd577acf218f973
Maria Constanza Flores <tatiflores.4@gmail.com>
2015-09-28 23:50:09 -0300

    Se añade la funcion nmasuno que cumple con lo requerido en la pregutna 1

--- tarea2.py
+++ tarea2.py
@@ -7,84 +7,76 @@ import numpy as np
 from scipy.optimize import brentq
 import matplotlib.pyplot as plt
 
-def y_suelo(A,w,t):
+def y_suelo(w,t):
     '''
     Ecuacion de posicion de la pelota en funcion del tiempo
     '''
+    A=1
     return A * np.sin (w*t)
 
-def v_suelo(A,w,t):
+def v_suelo(w,t):
+    '''
+    Ecuación de la velocidad del suelo en funcion del tiempo
+    '''
+    A=1
     return A * w * np.cos(w*t)
 
-def v_pelota(v0,g,t):
+def v_pelota(v0,t):
+    '''
+    Ecuacion de la velocidad de la pelota en funcion del tiempo
+    '''
+    g=1
     return v0-gt
 
-def y_pelota(y0,v0,t,g):
+def y_pelota(y0,v0,t):
     '''
     Ecuacion de posicion de la pelota en funcion del tiempo
     '''
+    g=1
     return y0 + v0 * t - 0.5 * g * t**2
 
-def resta(A,w,t,y0,v0,g):
-    return y_suelo(A,w,t) - y_pelota(y0,v0,t,g)
+def distancia(w,t,y0,v0):
+    '''
+    Retorna la distancia que hay entre la pelota y el suelo en un tiempo t
+    '''
+    A=1
+    g=1
+    return y_suelo(w,t) - y_pelota(y0,v0,t)
 
-A=1
-g=1
-n=1
-y0=0
-N=100
-t=np.linspace(0,np.pi/4, N)
-v0=10
+def v_despreb(n, w, t_reb, v0):
+    ''''
+    Retorna la velocidad justo despues del rebote
+    '''
+    A=1
+    g=1
+    return (1 + n)*v_suelo(w, t_reb) - n * v_pelota(v0,t_reb)
+
+def nmasuno (w, n, t_reb_n, yn, vn):
+    '''
+    Retorna los valores de Yn+1 y Vn+1 dados Yn y Vn (la posicion y
+    velocidad luego del n-esimo choque). Tambien retorna el valor t*,
+    es decir, el tiempo en el que ocurre el choque.
+    '''
+    def d(t):
+        #Necesito que quede solo en funcion de t para poder usar brentq
+        return distancia (w,t,yn,vn)
+
+    epsilon=0.001
+    t_a = t_reb_n + epsilon #asi empiezo a recorrerde despues del tiempo del rebote n
+    t_b = t_reb_n
+
+    while d(t_a) >=0:
+        t_a+=epsilon
+        t_b+=epsilon
+
+    t_reb_n1 = brentq (resta, t_a, t_b)  #tiempo del rebote n+1
+    #las funciones de posicion estan definidas para un tiempo
+    #que se reinicia tras cada rebote
+    yn1 = y_pelota (yn, vn, t_reb_n1-t_reb_n)
+    vn1 = v_despreb(n, w, t_reb_n1, vn)
+    return [t_reb_n1, yn1, vn1]
+
+
+n=0.15
 w=1.66
-
-def v_despreb(n, A, w, t_reb, v0, g):
-    return (1 + n)*v_suelo(A, w, t_reb) - n * v_pelota(v0,g,t_reb)
-
-#para usar el metodo de brentq necesito entregarle una funcion,
-#un tiempo anterior al cero y un tiempo posterior al cero.
-#por eso debo definirlos (t_a y t_b)
-
-#t_a=0
-#t_b=10
-
-#t_reb = brentq(resta, t_a, t_b)
-
-arreglo_posc_pel=np.zeros(N-1)
-REB=0 #numero rebotes
-
-'''for i in range(N):
-    if resta(A,w,t[i],y0,v0,g)*resta(A,w,t[i+1],y0,v0,g) <=0:
-        t_reb = brentq (resta, t[i], t[i+1])
-        y0 = y_pelota (y0,v0,t_reb,g)
-        v0 = v_despreb(n,A,w,r_reb,v0,g)
-        REB+=1
-        arreglo_posc_pel[i]= y0
-
-    else:
-        arreglo_posc_pel[i] = y_pelota(y0,v0,t_reb,g)
-
-plt.figure(1)
-plt.clf()
-plt.plot(t, y_suelo, label='Suelo')
-plt.plot(t, arreglo_posc_pel, label='Pelota')
-
-plt.xlabel('tiempo')
-plt.ylabel('posicion')
-plt.legend()
-
-show()
-'''
-
-for i in range(N-1):
-    if resta(A,w,t[i],y0,v0,g)*resta(A,w,t[i+1],y0,v0,g) <=0:
-        t_reb = brentq (resta, t[i], t[i+1])
-
-plt.figure(1)
-plt.clf()
-plt.plot(t, y_suelo, label='Suelo')
-plt.plot(t, arreglo_posc_pel, label='Pelota')
-plt.axvline(t_reb,color='g')
-
-plt.xlabel('tiempo')
-plt.ylabel('posicion')
-plt.legend()
+y0=0
