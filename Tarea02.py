0d8e390fe8367e802cf664d62ee1ee9baf5da6a1
maxidirk <maxidirk@yahoo.es>
2015-09-29 14:45:18 -0300

    se trata de definir la posicion, hay problemas

--- Tarea02.py
+++ Tarea02.py
@@ -9,9 +9,10 @@ Maximiliano Dirk Vega Aguilera
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import integrate as Int
 
 #######################################################
-
+'''
 #R = lambda x : A*sin(w*x) + A*cos(w*x)
 
 
@@ -50,6 +51,7 @@ for i in range(len(dt)):
 plt.plot(dt,R_p(V1,dt))
 plt.show()
 
+'''
 ''''
 ver que como implementar correctamente:
 tengo la posicion y la velocidad del suelo
@@ -61,3 +63,34 @@ intersectar la posicion de la particula con la del suelo
 usar ese tiempo y repetir n veces
 buena suerte D:
 '''
+
+A = 1.  #Amplitud
+w = 1.5  #Frecuencia del suelo
+n = 0.3  #coeficiente de restitucion
+m = 1.  #masa
+g = 1.  #aceleracion de gravedad
+
+ti = 0  #tiempo inicial
+tf = 10 #tiempo final
+h = 0.001 #paso
+
+dt = np.arange(ti, tf, h) #intervalo de tiempo discreto
+
+V_pa= np.zeros(len(dt))  #velocidad antes del choque
+V_pd= np.zeros(len(dt))  #velocidad luego del choque
+R_p= np.zeros(len(dt))  #posicion de la particula
+V_p= np.zeros(len(dt))  #velocidad de la particula
+
+fR_s = lambda x : A*np.sin(w*x) #Posicion del suelo, x variable tiempo
+fV_s = lambda x : A*w*np.cos(w*x) #Velocidad del suelo, x variable tiempo
+fR_p = lambda V: V*dt -(1/2)*g*(dt**2) #posion de la particula,x tiempo, Vel despues del impacto
+fV_p1 = lambda x, h : (1+n)*V_s(x) - n*V_p(x-h) #Velocidad de la particula despues del impacto
+
+V_pa[0] = 5 #velocidad inicial antes del primer impacto (inicial)
+V_pd[0] = (1+n)*fV_s(0) - n*V_pa[0] #velocidad despues del primer impacto (inicial)
+R_p[0] = lambda x,i : V_pd[i]*x -(1/2)*g*(x**2)
+#R_p = Int.quad(V_pd[0],ti,tf)
+print V_pd[0]
+
+plt.plot(dt,R_p[0](dt,0))
+plt.show()
