5929b209bcc35fca1d8fbdacf62c5867fd329b79
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-09-30 15:14:21 -0400

    Implementada parte 3

--- new1.py
+++ new1.py
@@ -2,6 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import optimize as opt
 
+'''
+La funcion choque entrega la posicion yn+1 y vn+1 en base a y y v.
+Ademas, grafica la interseccion de las funciones. Esto permite comprobar que
+para cualquier 
+'''
 
 
 def choque(vn,yn):
@@ -14,11 +19,13 @@ def choque(vn,yn):
     ys= lambda x: A*np.sin(w*x)
     yp= lambda x: (((1/2.)*g*x**2)+vn*x+yn)
     y= lambda x: yp(x)-ys(x)
+    vs= lambda x: A*np.cos(w*x)
+    vp= lambda x: (g*x+vn)
     a=-vn/g
     b=(-vn-(vn**2-2*g*(yn+A))**(0.5))/g
-    s = opt.brentq(y,a,b)
-    return s
-
+    s = opt.bisect(y,a,b)
+    v=(1+n)*vs(s)-n*vp(s)
+    return s,v
 
 A=1
 g=-1
@@ -26,12 +33,14 @@ n=1
 t_0=0
 n=0.5
 w=1
-yn=17
-vn=1
+yn=1
+vn=4
 a=-vn/g
 b=(-vn-(vn**2-2*g*(yn+A))**(0.5))/g
 t=np.linspace(yn, b, 100)
-z=choque(vn,yn)
+zn=choque(vn,yn)
+z=zn[0]
+zz=zn[1]
 ys= lambda x: A*np.sin(w*x)
 yp= lambda x: (((1/2.)*g*x**2)+vn*x+yn)
 y= lambda x: yp(x)-ys(x)
@@ -48,3 +57,4 @@ plt.draw()
 plt.show()
 
 print z
+print zz
---
+++ new2.py
@@ -0,0 +1,86 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import optimize as opt
+
+
+
+def choque(vn,yn):
+    A=1
+    g=-1
+    n=1
+    t_0=0
+    n=0.15
+    w=1.66
+    ys= lambda x: A*np.sin(w*x)
+    yp= lambda x: (((1/2.)*g*x**2)+vn*x+yn)
+    y= lambda x: yp(x)-ys(x)
+    vs= lambda x: A*np.cos(w*x)
+    vp= lambda x: (g*x+vn)
+    a=-vn/g
+    b=(-vn-(vn**2-2*g*(yn+A))**(0.5))/g
+    s = opt.bisect(y,a,b)
+    v=(1+n)*vs(s)-n*vp(s)
+    return v,s
+
+
+
+plt.figure(1)
+plt.clf()
+
+N=50
+v=[]
+y=[]
+v0=9
+y0=0
+v=np.append(v,v0)
+y=np.append(y,y0)
+i=0
+n=[]
+n=np.append(n,i)
+while i <= N:
+    yn=y[i]
+    vn=v[i]
+    z=choque(vn,yn)
+    v_m=z[0]
+    y_m=z[1]
+    v=np.append(v,v_m)
+    y=np.append(y,y_m)
+    i=i+1
+    n=np.append(n,i)
+
+'''
+para graficar el n de relajo para varios valores de v0
+v0_n=(2, 5, 10, 20, 30, 40)
+k=1
+for v0 in v0_n:
+    N=50
+    v=[]
+    y=[]
+    y0=0
+    v=np.append(v,v0)
+    y=np.append(y,y0)
+    i=0
+    n=[]
+    n=np.append(n,i)
+    while i <= N:
+        yn=y[i]
+        vn=v[i]
+        z=choque(vn,yn)
+        v_m=z[0]
+        y_m=z[1]
+        v=np.append(v,v_m)
+        y=np.append(y,y_m)
+        i=i+1
+        n=np.append(n,i)
+    plt.subplot(611)
+    plt.plot(n,v,'g')
+    k=k+1
+
+'''
+
+
+plt.plot(n,v,'g')
+plt.draw()
+plt.show()
+print v
+print n
