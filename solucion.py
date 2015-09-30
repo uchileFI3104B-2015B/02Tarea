db43f81d853dca1386bcd0b402e1a5190bd5c015
brevadiaz <eva.diaz.g@ing.uchile.cl>
2015-09-30 00:26:10 -0300

    Otro intento no tan exitoso

--- solucion.py
+++ solucion.py
@@ -10,7 +10,8 @@ w=1
 rest=0.5 #va de 0 a 1
 y_0=0
 v_0=2 #debe ser mayor que A*w
-
+n=10 #número de rebotes
+times=[]; #vector que contendrá los tiempos en que se producen los rebotes
 
 def floor(t):
     y_s=A*np.sin(w*t); #posición del suelo
@@ -24,19 +25,27 @@ def floorv(t):
 def particlev(t):
     v_p=v_0-g*t #velocidad de la partícula
     return v_p
+def collision(t):
+    v_p_new=(1+rest)*floorv(t)-rest*particlev(t) #cambio de velocidad de la partícula al chocar
+    return v_p_new
 def resta(x):
     y=floor(x)-particle(x)
     return y
-zero1=opt.bisect(resta,4,6);
-for i in range():
-    t_new=zero1
-    v_p_new=(1+rest)*floorv(zero1)-rest*particlev(zero1) #velocidad de la particula despues de chocar por 1ra vez
-
 
 plt.figure(1)
 plt.clf()
-plt.plot(t,floor(t),'r',t,particle(t),'b')
+
+for i in range(0,n):
+    zero_i=opt.bisect(resta,4+i,6+i); #falta determinar los instantes entre los cuales la wea choca
+    times.insert(i,zero_i) #inserta el tiempo en que ocurre el choque i
+    v_p_new=(1+rest)*floorv(zero_i)-rest*particlev(zero_i) #velocidad de la particula despues de chocar por 1ra vez
+    y_0_new=particle(zero_i)
+    def particle(t):
+        y_p=(y_0_new)+(v_p_new*(t-zero_i))-((1/2.)*g*(t-zero_i)**2); #redefino la ecuación de movimiento de la partícula
+        return y_p
+    plt.plot(t,floor(t),'r',t,particle(t),'b')
+    plt.axvline(zero_i,color='g')
+
 plt.xlabel('Tiempo [s]')
 plt.ylabel('Altura [m]')
-plt.axvline(zero1,color='g')
 plt.show()
