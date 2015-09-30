bc7de26b51c98f8f2da5815b9e6486b8a82f283e
benjammin94 <benja_hunter_x@hotmail.com>
2015-09-30 14:20:07 -0300

    Update 3
    Se agrego iteracion (infuncional por el momento) y grafico del segundo
    salto. Se tiene el conflicto de la identificacino numerica de la
    interseccion del segundo salto.

--- codigotarea2.py
+++ codigotarea2.py
@@ -4,6 +4,8 @@ Este script...
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import brentq
+from scipy.optimize import newton
+from scipy.optimize import bisect
 m=1
 g=1
 A=1
@@ -27,15 +29,40 @@ raiz_brent=brentq(resta , 0.1, 10.5, args=(0,5) ) #(arreglar v0)Encontramos la
 def v_p_choque(t,v_0):              #Definimos la funcion de velocidad de la
     return (1+n)*v_s(t)-n*v_p(t,v_0)#particula despues del choque.
 
+def h_max(y_0,v_0):
+    return y_0+(v_0**2)/(4*g)
+
+def t_viaje(y_0,v_0):
+    return v_0+(v_0**2+2*(1+y_0))**0.5
+
 print ('Tiempo en el que chocan la particula con el suelo')
 print raiz_brent                    #Mostramos el tiempo de interseccion
 print ('Velocidad de la particula despues del choque')
 print v_p_choque(raiz_brent,5)      #(arreglar v0) Mostramos el valor de la
                                     #velocidad despues del choque
+vpchoque=v_p_choque(raiz_brent,5)   #(arreglar v0)
+t_inter=raiz_brent
+while (vpchoque > 0):
+    yp=y_p(t_inter , y_s(t_inter) ,vpchoque )
+    ys=y_s(t_inter)
+    vp=v_p(t_inter,vpchoque)
+    vs=v_s(t_inter)
+    #t_inter+=brentq(resta , t_inter+0.1 , t_viaje(yp,vp)+0.1 , args=(yp,vp))
+    t_inter=newton(resta , t_viaje(yp,vp) , args=(yp,vp) )
+    vpchoque=v_p_choque(t_inter,vp)
+    print ('velocidades de choque')
+    print vpchoque
+    print ('tiempos')
+    print t_inter
 
+print ('interseccion del segundo rebote')
+print raiz_brent+newton(resta,18,args=(0.73,4.41))
+t_02=np.linspace(raiz_brent,20,200)
+t_03=np.linspace(0,20,400)
 plt.figure(1)
 plt.clf()
-plt.axvline(raiz_brent)
-plt.plot(t_0,y_p(t_0,0,5))          #arreglar v0
-plt.plot(t_0,y_s(t_0))
+#plt.axvline(raiz_brent)
+plt.plot(t_0,y_p(t_0,0,5))          #(arreglar v0)
+plt.plot(t_02,y_p(t_0,0.73,4.41))
+plt.plot(t_03,y_s(t_03))
 plt.show()
