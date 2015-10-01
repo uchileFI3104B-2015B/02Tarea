ceb8a67289e91102b25f898600e3207c8c9fba95
hpozojose <hpozo.jose@gmail.com>
2015-10-01 20:25:31 -0300

    Agrega variacion de la velocidad y n de relajacion dependiendo de la frecuencia

--- Codigo.py
+++ Codigo.py
@@ -10,9 +10,9 @@ from scipy.optimize import newton
 plt.figure(1)
 plt.clf()
 #definimos una funcion de posicion y velocidad del suelo
-def y_suelo(A, w, t):
+def y_suelo(w, t):
     return A*np.sin(w*t)
-def v_suelo(A, w, t):
+def v_suelo(w, t):
     return A*w*np.cos(w*t)
 #definimos funciones de posicion y velocidad para la pelota
 def y_pelota(y0, v0, t):
@@ -20,56 +20,67 @@ def y_pelota(y0, v0, t):
 def v_pelota(v0, t):
     return v0 -g*t
 # p menos s calcula la distancia entre la pelota y el suelo
-def p_menos_s(y0, v0, t, A, w):
-    return y_pelota(y0, v0, t) - y_suelo(A, w, t)
+def p_menos_s(y0, v0, t, tn, w):
+    return y_pelota(y0, v0, t-tn) - y_suelo(w, t)
 # v choque es la velocidad despues del choque
 def v_choque(vs, vp):
-    return (1+nu)*vs-nu*vp
+    return (1+n)*vs-n*vp
 
 
-def N(Yn,Vn,w,A,tn):
+def N(Yn,Vn,w,tn):
     '''
-    Devuelve la posicion(Y_{n+1}) y la velocidad(V_{n+1})
-    de la particula luego del choque n ademas del tiempo de choque
-    en un vector de la forma [Yn+1,Vn+1,t]
+    Devuelve la posicion y velocidad del proximo choque
+    dandole la velocidad, la posicion y tiempo del choque
+    anterior
     '''
     def p_s(t):
-        d=p_menos_s(y0, v0, t, A, w)
+        d=p_menos_s(Yn, Vn, t, tn, w)
         return d
 
-    delta=0.001
-    t1=tn+delta
-    t2=tn+2*delta
-    while (p_s(t1)*p_s(t2)>0.0):
-            t1=t1+delta
-            t2=t2+delta
-
-    tn1 = brentq(p_s,t1,t2)
-    Yn1 = y_pelota(tn1-delta,Yn,Vn)
-    vs = v_suelo(tn1,w,A)
-    vp = v_pelota(tn1-delta,Vn)
+    delta=0.01
+    t1=tn
+    p1=p_s(t1)
+    p2=p_s(t1+delta)
+    #print("Pos1: "+str(p1)+" Pos2: "+str(p2))
+    while p2>0.0:
+        t1=t1+delta
+        p1=p2
+        p2=p_s(t1+delta)
+        #print("Pos1: "+str(p1)+" Pos2: "+str(p2))
+    if p1>0.0:
+        tn1 = brentq(p_s,t1,t1+delta)
+    else:
+        tn1 = t1
+    Yn1 = y_pelota(Yn,Vn,tn1-tn-delta)
+    vs = v_suelo(w, tn1-delta)
+    vp = v_pelota(Vn, tn1-tn-delta)
     Vn1 = v_choque(vs,vp)
     r=[Yn1,Vn1,tn1]
     return r
 #definimos las variables de las cuales depende el problema
 m=1
 A=1
-w=1
-nu=1
+w=1.7
+n=0.6
 y0=0
 v0=2
 g=1
-def yp(t):
-    return y0 + v0*t -(g*(t**2))/2
-def ys(t):
-    return A*np.sin(w*t)
-Choque_n1=N(y0,v0,w,A,0)
-print Choque_n1
 
-xt = np.linspace(0, 4*np.pi, 100)
-plt.plot(xt, ys(xt), label='suelo')
-plt.plot(xt, yp(xt), label='pelota')
+def Nchoque(w, c):
+    i=0
+    Y=[0]
+    V=[2]
+    T=[0]
+    while i<c:
+        Ni=N(Y[i],V[i],w,T[i])
+        T.append(Ni[2])
+        V.append(Ni[1])
+        Y.append(Ni[0])
+        i+=1
+        print Ni
+    return V
+Speed=Nchoque(1, 30)
 
-plt.xlabel('x [segundos]')
-plt.legend()
+tiempo= np.linspace(0,30,31)
+plt.plot(tiempo, Speed)
 plt.show()
