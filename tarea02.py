e7a55bac4b101be079e47ea09ad9a9bd50fe8ace
Rodrigo Soria R <rodro077@live.cl>
2015-10-01 01:18:58 -0300

    avance2

--- tarea02.py
+++ tarea02.py
@@ -9,21 +9,23 @@ g = -1 ; eta = 0.5 ; A = 1 ; w = 2
 desfase = 0	 ; t_inicio = 0  ; delta_t = 0.1  
 t_final = 100 
 t = np.arange(t_ini, t_fin, delta_t)
-
-'''
-Código para el movimiento del suelo
-'''
+pos_particula = np.zeros(len(t))
+velP_particula = np.zeros(len(t))
+velP*_particula = np.zeros(len(t))
 pos_suelo = np.zeros(len(t))
 vel_suelo = np.zeros(len(t))
+'''
+Código para calcular el movimiento del suelo y la particula
+'''
+
 for i in range(0,len(t)-1):
     pos_suelo[i] = A*sin(w*t[i])
     vel_suelo[i] = A*w*cos(w*t[i])
-    
-    
-def particula(t):
-    pass
+    pos_particula[i] = 
+    velP_particula[i] =(g*t[i]**2)/2
+    if pos_particula[i]==pos_suelo[i]:
+        velP*_particula[i] = (1+eta)*vel_suelo[i]-eta*velP_particula[i]
+'''
 
-def choque(A,w,t):
-    pass
-
+'''
 
