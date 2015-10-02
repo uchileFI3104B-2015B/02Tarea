a34a5edbd481e47525ec589a51d0ff06489cd1ad
[Felipe Castillo] <felipe.castillo.t@ing.uchile.cl>
2015-10-02 18:40:44 -0300

    Parte 1 finalizada

--- Tarea2.py
+++ Tarea2.py
@@ -7,27 +7,16 @@ plt.rc('font', family='serif')
 #### Parametros del problema
 
 A = 1.
-w = np.pi
+w = np.pi/10
 m = 1.
 g = 1.
-VPF = 0
-VP0 = 4
+VP0 = np.pi #w
 n = 0.4
-VS = 0
-Y0 = 0
-t = np.linspace(0,10,10000)
-###
-Y = Y0 + VP0*t - (1/2.)*g*t**2
-#VF = (1+n)VS -nVP0
-YP = A*np.sin(w*t)
-VP = A*w*np.sin(w*t)
-
-def PosicionRelativa(t, PosicionN, VelocidadN,phi):
-    Yrelativa = PosicionN + VelocidadN*t - (1/2.)*g*t**2 -
 
 
 
-def SiguienteChoque(PosicionN, VelocidadN, N, SubiendoBajando):
+
+def SiguienteChoque(PosicionN, VelocidadN, SubiendoBajando):
     '''
     '''
     phi = np.arcsin(PosicionN)
@@ -36,17 +25,29 @@ def SiguienteChoque(PosicionN, VelocidadN, N, SubiendoBajando):
     elif SubiendoBajando == "Bajando":
         phi = np.pi-phi
     else:
-        return print 'Entrada invalida, entre "Subiendo" o "Bajando"'
+        return 'Entrada invalida, entre "Subiendo" o "Bajando"'
 
+    def PosicionRelativa(t):
+        Yrelativa = PosicionN + VelocidadN*t - (1/2.)*g*t**2 - np.sin(w*t+phi)
+        return Yrelativa
 
-
-    phi = np.arcsin(PosicionN)
-    VS=-1*w*np.cos(wt)
-
-    VPF = (1+n)*VS - n*VelocidadN
     #buscar el zero
-    Raiz_brentq = brentq()
-    YParticula = PosicionN + VelocidadN*t -(1/2.)*g*t**2
-    Yaux = YParticula-Ysuelo
-    #buscar cero de Yaux con intervalo PosicionN y y=-1 para la particula
-    return YPF,VPF #tercer parametro que dice si el suelo termino subiendo o Bajando
+    tmax = 2*VelocidadN/g +(-VelocidadN+np.sqrt(VelocidadN**2 + 2*g*(PosicionN+1)))/g
+    Raiz_brentq = brentq(PosicionRelativa,0.1,tmax)
+
+    YN1 = np.sin(w*(Raiz_brentq)+phi)
+    VN1 = (1+n)*(w*np.cos(w*(Raiz_brentq)+phi)) - n*(VelocidadN-g*Raiz_brentq)
+    print "piso ", (w*np.cos(w*(Raiz_brentq)+phi)), "Pelota", VelocidadN-g*Raiz_brentq, "tiempo", Raiz_brentq
+    SoB = ""
+    if np.cos(w*Raiz_brentq+phi)>0:
+        SoB = "Subiendo"
+    elif np.cos(w*Raiz_brentq+phi)<0:
+        SoB = "Bajando"
+    elif np.cos(w*Raiz_brentq+phi)==0 and YN1>0:
+        SoB = "Subiendo"
+    elif np.cos(w*Raiz_brentq+phi)==0 and YN1<0:
+        SoB = "Bajando"
+    return YN1, VN1, SoB
+
+Esto = SiguienteChoque(0,VP0,"Subiendo")
+print Esto
