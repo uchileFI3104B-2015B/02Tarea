d0efc7b9aa56e6c23e8f6219a9f150ed9615ec15
sebastiantp <sebastian.toro@ug.uchile.cl>
2015-10-01 23:06:28 -0300

    altualuizacion

--- tarea2.py
+++ tarea2.py
@@ -1,44 +1,45 @@
 #! /usr/bin/env python
 
-
+#este es un script que modela el rebote de una pelota
+#sobre una superficie que se mueve con una funcion sinusoidal
 
 
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import scipy.optimize
 from scipy.optimize import brentq
 
-def definiciones():  #crea variables globales para todas las funciones
+def definiciones(): #crea las variables globales para todas la funciones
     global pos_init
     pos_init = 0
     global vel_init
     vel_init = 15
     global w
-    w= 1.68
+    w= 1.79
     global ajt
     ajt = 0
 
 
-def resta(time):     #recibe variables globales y calcula la distancia relativa entre el suelo y la pelota
+def resta(time):   #calcula la distancia relaiva entre el suelo y la pelota
     global pos_init
     global vel_init
     global w
     return pos_init + (vel_init*tiempoajustado(time)) - (tiempoajustado(time)*tiempoajustado(time)/2) - np.sin(w*time)
 
-def tiempoajustado(t):  #crea la variable global de tiempo ajustado que sera el nuevo intervalo de tiempo
+def tiempoajustado(t):  #ajuste de tiempo
     global ajt
     return t - ajt
 
-def ajustartiempo(t):  #
+def ajustartiempo(t):  #ajuste temporal
     global ajt
     ajt = t
 
 
-def choque(coef,velp,vels):        #calcula la velociadad de la pelota despues del choque
+def choque(coef,velp,vels):   #calcula la velocidad despues del rebote
     return ((1+coef)*vels) - (coef*velp)
 
 
-def func_cinm(Ns,coef):
+def func_cinm(Ns,coef):  #funcion que describe la cinematica del problema
     global vel_init
     global pos_init
 
@@ -65,56 +66,42 @@ def func_cinm(Ns,coef):
 
 
 
-        tiempo.append(brentq(resta,t-epsilon,t))   #guarda el instante del choque
-
+        tiempo.append(brentq(resta,t-epsilon,t))
         t=tiempo[nchoques]
-
-
-
-
-
-
         vp=(vel_init)-(tiempoajustado(t))
         vs=w*np.cos(w*t)
-
-
-        vel.append(choque(coef,vp,vs))     #guarda la velocidad con que reboto la pelota
-
-
+        vel.append(choque(coef,vp,vs))
         pos_init= np.sin(w*t)
         vel_init= vel[nchoques]
-
         ajustartiempo(t)
-
-        print pos_init,vel_init,nchoques
-
+        print vel_init,nchoques
         nchoques+=1
-
         t=t+epsilon
         dato=resta(t)
 
-    return tiempo,vel
+    return tiempo , vel
 
 
 definiciones()
 
-# func_cinm(500,0.15)
 
-a,b = func_cinm(50,0.15)
-print a
+
+a,b = func_cinm(80,0.15)
+
+
+
+x_values = np.linspace(1, 80, 80)
+print x_values
+plt.figure(1)
+plt.clf()
+print "velocidades"
 print b
+plt.plot(x_values, b , label="w = 1,7 " )
 
 
-#plt.figure(1)
-#plt.clf()
-
-#x_values = np.linspace(0, np.pi, 100)
-#plt.plot(x_values, np.sin(x_values), label='sin(x)')
-#plt.plot(x_values, np.cos(x_values), label='cos(x)')
-
-#plt.xlabel('x [radianes]')
-#plt.legend()
+plt.xlabel('numero de rebote')
+plt.legend()
 
 
-#plt.draw()
-#plt.show()
+plt.draw()
+plt.show()
